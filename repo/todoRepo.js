5a429e0cf254c12c34a4bc92f8ef2a52a851fb1e
Tamara-Sarabteh <tamara.sarabteh@optimumpartners.co>
2021-01-26 01:28:21 +0200

    finalllll

--- repo/todoRepo.js
+++ repo/todoRepo.js
@@ -10,9 +10,9 @@ module.exports = {
       name: name,
       complete: complete,
     }),
-  updateTodo: async (todoid, { name , complete }) =>
-    Todo.updateMany({ _id : todoid } ,{name : name , complete : complete} ) ,
-  deleteTodo: async (todoid) => Todo.deleteOne({ _id : todoid })
-     }  
+  updateTodo: async (todoid, { name, complete }) =>
+    Todo.updateMany({ _id: todoid }, { name: name, complete: complete }),
+  deleteTodo: async (todoid) => Todo.deleteOne({ _id: todoid }),
+};
 
-     //await Person.replaceOne({ _id }, { name: 'Jean-Luc Picard' })
+//await Person.replaceOne({ _id }, { name: 'Jean-Luc Picard' })
